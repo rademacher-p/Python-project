f5c91f2f3963d654421f73c2dc572966358a832d
Paul Rademacher <paul.rademacher@nrl.navy.mil>
2024-01-16 19:45:46 -0500

    pytest vs unittest exploration

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,16 +1,11 @@
 from python_project import hello_world, say_hello
 
 
+def test_say_hello():
+    name = "you"
+    assert say_hello(name) == f"Hello, {name}!"
+
+
 def test_true():
     hello_world()
     assert True
-
-
-def test_false():
-    hello_world()
-    assert not False
-
-
-def test_hello():
-    name = "you"
-    assert say_hello(name) == f"Hello, {name}!"
