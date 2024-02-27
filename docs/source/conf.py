877064c6be7ed599c94f782518077e09e29f8485
Paul Rademacher <paul.rademacher@nrl.navy.mil>
2024-02-27 11:03:52 -0500

    Sphinx files init

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,32 +1,18 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Python-project"
-copyright = "2023, Paul Rademacher"
+copyright = "2024, Paul Rademacher"
 author = "Paul Rademacher"
 
-
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
@@ -44,27 +30,24 @@ imgmath_latex_preamble = r"""
 \usepackage{amsmath,amssymb,amsfonts,amsthm}
 \usepackage[retainorgcmds]{IEEEtrantools}
 """
+# imgmath_latex_preamble = "\n".join(
+#     [
+#         r"\usepackage{amsmath,amssymb,amsfonts,amsthm}",
+#         r"\usepackage[retainorgcmds]{IEEEtrantools}",
+#     ]
+# )
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns: list = []
+templates_path: list[str] = ["_templates"]
+exclude_patterns: list[str] = []
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# html_theme = "alabaster"
 html_theme = "furo"
 # html_theme = "sphinx_rtd_theme"
 # html_theme = "pydata_sphinx_theme"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path: list = []
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
+# html_static_path: list[str] = []
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Python-project documentation master file, created by
+   sphinx-quickstart on Tue Feb 27 09:43:01 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Python-project's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
